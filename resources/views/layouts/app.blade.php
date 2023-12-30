df815204110f59e948d4b28bc318263ea35a3c8b
JacqueRobles <valeria.robles.tesillo@gmail.com>
2023-12-30 00:19:11 -0300

    Add preline import and update app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,31 +3,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/css/app.css')
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-                           
-
+    
+    
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
-    <div class="bg-teal-400 dark:bg-gray-900">
+    <div class="bg-teal-400 dark:bg-gray-900 min-w-screen min-h-screen h-screen flex flex-col">
      <header class="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700 text-sm py-3 sm:py-3 dark:bg-clip-padding dark:backdrop-filter dark:backdrop-blur-sm dark:bg-opacity-20">  
         <nav class="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
             <div class="flex items-center justify-between">
                 <a class="flex-none text-xl font-semibold dark:text-white" href="{{ url('/') }}" aria-label="Brand">
                     Pet Club
                 </a>
-                <div class="sm:hidden" x-data="{open: false, toggle() { this.open = ! this.open }">
-                    <button @click="toggle()" type="button" class="hs-collapse-toggle w-9 h-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
+                <div class="sm:hidden">
+                    <button type="button" class="hs-collapse-toggle w-9 h-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
                       <svg class="hs-collapse-open:hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
                       <svg class="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                     </button>
@@ -35,15 +36,18 @@
             </div>
             <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
                 <!-- Left Side Of Navbar -->
-                <div class="flex flex-col gap-y-4 gap-x-0 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:ps-7">
+                <div class="flex flex-col w-full sm:justify-between gap-y-4 gap-x-0 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:ps-7">
                     @auth
-                    <a class="font-medium text-blue-600 sm:py-6 dark:text-blue-500" href="/admin" aria-current="page">Dashboard</a>
-                    <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admin/news">News</a>
-                    <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/animals">Animals</a>
-                    <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admin/contacts">Contacts</a>
+                    <div class="flex gap-5">
+                        <a class="font-medium text-blue-600 sm:py-6 dark:text-blue-500" href="/admin" aria-current="page">Dashboard</a>
+                        <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admin/news">News</a>
+                        <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/animals">Animals</a>
+                        <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admin/contacts">Contacts</a>
+                    </div>
+                    @endauth
+                    @guest
                     <ul class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7 w-full">
                         <!-- Authentication Links -->
-                    @guest
                         @if (Route::has('login'))
                             <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="{{ route('login') }}">{{ __('Login') }}</a>
                         @endif
@@ -65,13 +69,12 @@
                         </li>
                     @endguest
                     </ul>
-                    @endauth
                 </div>
                     <!-- Right Side Of Navbar -->
             </div>
         </nav>
     </header>
-    <main>
+    <main class="flex-1">
         @yield('content')
     </main>
     <div>
--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="flex justify-center items-center text-black pt-5">
-    <div class="w-5/6 bg-white h-screen">
+<section class="flex justify-center items-center h-full text-black py-5">
+    <div class="w-5/6 bg-white h-full">
         <div class="flex h-1/2">
             <div class="w-1/3 bg-green-300 m-5">hello</div>
             <div class="w-2/3 bg-purple-300 m-5">
@@ -12,13 +12,11 @@
                 </form>
             </div>
         </div>
-        <div class="bg-red-400 w-full h-1/2 p-5">
-            <div class="bg-cyan-200 size-full">
+        <div class="bg-red-400 w-full h-1/2">
             <p>{{ $pet }}</p>
             <p>Age: {{ $pet }}</p>
             <p>Species: {{ $pet }}</p>
             <p>Breed: {{ $pet }}</p>
-            </div>
         </div>
     </div>
 </section>
