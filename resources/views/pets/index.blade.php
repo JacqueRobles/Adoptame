62e872e6be6b3fda80c2ee2d808e6d62fe8801af
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-01-02 03:06:58 -0300

    Refactor pet index view

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -7,42 +7,24 @@
             <h1 class="text-2xl">Mascotas en adopción</h1>
 
             <br> 
-            {{-- @if (User::hasRole('organization')) --}}
+            
             
             {{-- <a class="cursor-pointer py-3 px-4 w-2/6 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-violet-600 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('pet.create') }}">
                 agregar mascota c:
             </a> --}}
          
-            @auth
+            @role('organization|admin')
             <a class="cursor-pointer mt-5 py-3 px-4 w-1/6 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-violet-600 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('pet.create') }}">
                 <button type="button" class="">Agregar Mascota</button> </a>
                 
-            {{-- @endif --}}
-            @endauth        
+            @endrole 
+                    
         </div>
         <!-- aca le puse h fit para que no se viera el espacio, despues le pones h-2/6 o algo asi-->
         <div class="mt-5 h-fit">
             @include('partials.card2', $pets)
 
         </div>
-        <table class="table table-bordered h-1/2">
-            <thead>
-                <th>Nombre Mascota</th>
-                <th>Tipo</th>
-                <thead>Organizacion (direccion)</th>
-                </thead>
-                <tbody>
-                    @foreach ($pets as $pet)
-                    <tr>
-                        <td> {{ $pet->nickname }}</td>
-                        <td> {{ $pet->type }}</td>
-                        <tD>{{ $pet->organization->name }}</tD>
-                    </tr>
-                    @endforeach
-
-                </tbody>
-        
-        </table>
     </div>
 </section>
         @endsection
