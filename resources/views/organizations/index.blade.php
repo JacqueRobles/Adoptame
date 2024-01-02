13999c055550bd1b15bac13e1036ec450c97e039
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-01-02 03:06:45 -0300

    Refactor organization index view

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -1,46 +1,23 @@
 @extends('layouts.app')
-
 @section('content')
-<section class= "text-white">
-    <h1>Soy el Index de Organization y debería mostrar todos los datos que hay</h1>
-    <hr>
-    <p>Se enchula se pone mas bonito bootrap, css grid, etc etc etc</p>
-    pero lo que importa es que <strong><b>FUNCIONE</b></strong>
-    ACA NO SE PREOCUPEN TANTO DE COMO LO MUESTRO, CADA LP TIENE SU MANERA DIFERENTE DE HACERLO, centrense más que estoy
-    enviando datos desde el controlador a la vista para que lo pueda ver el usuario
-    <hr>
+<section class="min-w-screen min-h-screen flex justify-center items-center h-screen text-white py-5">
+    <div class="w-5/6 h-full flex flex-col ">
+        <div class="p-5 h-1/6">
 
-    <h4>Las organizaciones inscristas en adoptame.cl son:</h4>
+            <h1 class="text-2xl">Organizaciones Participantes</h1>
+            <br>
+            {{-- @role('organization|admin')
+            <a class="cursor-pointer mt-5 py-3 px-4 w-1/6 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-violet-600 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('organization.create') }}">
+                <button type="button" class="">Agregar Organizacion</button> </a>
+                
+            @endrole  --}}
+                    
+        </div>
+        <!-- aca le puse h fit para que no se viera el espacio, despues le pones h-2/6 o algo asi-->
+        <div class="mt-5 h-fit">
+            @include('partials.card-organization', $organizations)
 
-    <a href="{{ route('organization.create') }}"><button type="button">Agregar Organizacion</button> </a>
-
-    <table class="table table-bordered">
-        <thead>
-            <th> Nombre</th>
-            <th> Direccion</th>
-            <th> Editar</th>
-            <th> Eliminar</th>
-        </thead>
-        @foreach ($organizations as $organization)
-            <tr>
-                <td>{{ $organization->name }}</td>
-                <td>{{ $organization->address }}</td>
-                <td><a href="{{ route('organization.edit', $organization->id) }}"><button
-                            type="button">Editar</button></a>
-                </td>
-                <td>
-                    <a onclick="confirm('¿Está seguro que desea eliminar la organizacion de adoptame.cl?')">
-                        <form action="{{ route('organization.destroy', $organization->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Eliminar</button>
-                        </form>
-                    </a>
-                </td>
-                </form>
-            </tr>
-        @endforeach
-    </table>
-
+        </div>
+    </div>
 </section>
-@endsection
+        @endsection
