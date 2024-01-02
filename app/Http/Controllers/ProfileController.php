580c96ff7af19f544b02d9a056101daf6f69f44a
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-01-02 03:05:57 -0300

    Add User model import to ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
-
+use App\Models\User;
 class ProfileController extends Controller
 {
     /**
@@ -57,4 +57,5 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
+
 }
